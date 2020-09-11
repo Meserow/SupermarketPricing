80b37c3463e067da257b1d311588e40be2da7eab
mpogson <mikep@meserow.com>
2020-09-11 14:17:50 -0700

    initial commit of supermarket pricing

---
+++ SupermarketPricing.Tests/CheckoutTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace SupermarketPricing.Tests
+{
+    public class CheckoutTests
+    {
+        private Checkout _checkout;
+
+        [SetUp]
+        public void BeforeEach()
+        {
+            _checkout = new Checkout();
+        }
+
+        [Test]
+        public void OneCanOfBeans_CostsOneDollar()
+        {
+            //Arrange
+            var beans = new Product
+            {
+                Name = "Beans",
+                Price = 1
+            };
+            
+            _checkout.Products.Add(beans);
+
+            //Act
+            var total = _checkout.CalculateNetTotal(); 
+
+            //Assert
+            total.Should().Be(1);
+        }
+    }
+}
---
+++ SupermarketPricing/Checkout.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace SupermarketPricing
+{
+    public class Checkout
+    {
+        public List<Product> Products { get; }
+
+        public Checkout()
+        {
+            this.Products = new List<Product>();
+        }
+
+        public decimal CalculateNetTotal()
+        {
+            return -1;
+        }
+    }
+}
